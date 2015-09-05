3c6ab8db7def78c8d263dcb153176f2e829126dc
Chaofang <chaofang.xie@gmail.com>
2015-09-04 22:05:56 -0700

    Changes to be committed: 	new file:   Echos.java

---
+++ Echos.java
@@ -0,0 +1,11 @@
+public class Echos{
+public static void main(String[] args){
+//System.out.println(args[0]);
+int sum = 0;
+for(int i = 0; i < args.length; i++){
+System.out.println(args[i]);
+sum +=Integer.parseInt(args[i]);
+}
+System.out.println(sum);
+}
+}
